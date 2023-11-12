705b68a049a268c87bc253baa73630967b8c6f8f
haruka03204869 <149073246+haruka03204869@users.noreply.github.com>
2023-11-13 01:00:20 +0900

    Update feedback.js

--- src/app/pages/feedback.js
+++ src/app/pages/feedback.js
@@ -1,1 +1,28 @@
+import React, { useState } from 'react';
+
+export default function FeedbackPage() {
+    const [feedback, setFeedback] = useState('');
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        // 这里添加发送反馈到后端的代码，例如使用fetch API
+        console.log(feedback);
+    };
+
+    return (
+        <div>
+            <h1>User Feedback</h1>
+            <form onSubmit={handleSubmit}>
+                <textarea
+                    value={feedback}
+                    onChange={(e) => setFeedback(e.target.value)}
+                    placeholder="Please enter your feedback here..."
+                    rows="4"
+                    required
+                />
+                <button type="submit">Submit Feedback</button>
+            </form>
+        </div>
+    );
+}
 
