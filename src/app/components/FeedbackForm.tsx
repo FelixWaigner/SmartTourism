405018f86f09db6aa2c5daa9bd1c34ebe1e8132c
blacfli <michaelevan.santoso65@gmail.com>
2023-11-14 18:25:56 +0900

    feedback updated

---
+++ src/app/components/FeedbackForm.tsx
@@ -0,0 +1,24 @@
+import { Button, Label, Textarea } from 'flowbite-react';
+import React, { useState } from 'react';
+
+const FeedbackForm = () => {
+  const [feedback, setFeedback] = useState('');
+
+  const handleSubmit = () => {
+    // Handle the form submission, e.g., send data to an API
+    console.log('Feedback submitted:', feedback);
+  };
+
+  return (
+    <div className="max-w-md container mx-auto">
+      <div className='text-2xl text-center font-bold'>Feedback Form</div>
+      <div className="mb-2 block">
+        <Label htmlFor="comment" value="Your comments" />
+      </div>
+      <Textarea value={feedback} onChange={e => setFeedback(e.target.value)} id="comment" placeholder="Leave a comment..." required rows={4} />
+      <Button onClick={handleSubmit} color='dark' className='mt-5'>Post comments</Button>
+    </div>
+  );
+};
+
+export default FeedbackForm;
---
+++ src/app/feedback/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+import React, { useState } from 'react';
+import FeedbackForm from '../components/FeedbackForm';
+
+
+
+export default function Feedback(){
+    return (
+        <div>
+            <FeedbackForm />
+        </div>
+      );
+}
