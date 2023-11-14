03c69de43cd6e0f719be56901880b2e0d942070e
Hachi <82988679+pichayakorn@users.noreply.github.com>
2023-11-14 20:06:08 +0900

    fix(feedbackform): handleSubmit err
    Fix error from handleSubmit using prevenDefault

--- src/app/components/FeedbackForm.tsx
+++ src/app/components/FeedbackForm.tsx
@@ -1,24 +1,38 @@
-import { Button, Label, Textarea } from 'flowbite-react';
-import React, { useState } from 'react';
+import { Button, Label, Textarea } from "flowbite-react"
+import React, { useState } from "react"
 
 const FeedbackForm = () => {
-  const [feedback, setFeedback] = useState('');
+  const [feedback, setFeedback] = useState("")
 
-  const handleSubmit = () => {
+  const handleSubmit = async (event: { preventDefault: () => void }) => {
+    event.preventDefault()
     // Handle the form submission, e.g., send data to an API
-    console.log('Feedback submitted:', feedback);
-  };
+    console.log("Feedback submitted:", feedback)
+  }
 
   return (
-    <div className="max-w-md container mx-auto">
-      <div className='text-2xl text-center font-bold'>Feedback Form</div>
-      <div className="mb-2 block">
-        <Label htmlFor="comment" value="Your comments" />
-      </div>
-      <Textarea value={feedback} onChange={e => setFeedback(e.target.value)} id="comment" placeholder="Leave a comment..." required rows={4} />
-      <Button onClick={handleSubmit} color='dark' className='mt-5'>Post comments</Button>
+    <div className="w-full mt-5 mb-24">
+      <form className="w-full" onSubmit={handleSubmit}>
+        <div className="mb-2 block">
+          <Label htmlFor="comment" value="Your comments" />
+        </div>
+        <Textarea
+          id="comment"
+          value={feedback}
+          onChange={(e) => setFeedback(e.target.value)}
+          placeholder="Please enter your feedback here..."
+          rows={4}
+          required
+        />
+        <button
+          className="w-full relative text-white bg-green-400 hover-bg-green-500 focus:outline-none focus:ring-4 focus-ring-green-300 font-medium rounded-full text-sm px-5 py-3.5 text-center dark-bg-blue-600 dark-hover-bg-blue-700 dark-focus-ring-blue-800 mt-5"
+          type="submit"
+        >
+          Submit Feedback
+        </button>
+      </form>
     </div>
-  );
-};
+  )
+}
 
-export default FeedbackForm;
+export default FeedbackForm
