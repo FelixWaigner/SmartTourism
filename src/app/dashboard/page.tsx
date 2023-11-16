52be4590f9b75d8b2c70ecb598a85a8774aeed38
Felix Waigner <Waigner.Felix@t-online.de>
2023-11-16 14:17:39 +0100

    Added dashboard page and navigation component

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import Menu from "../components/navigation/menu"
+
+export default function Dashboard() { 
+    return(
+        <div>
+            <h1>Dashboard</h1>
+            <Menu />
+        </div>
+    )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,10 +2,10 @@ import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
+    "./node_modules/flowbite-react/**/*.js",
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    "./node_modules/flowbite-react/**/*.js",
   ],
   theme: {
     extend: {
@@ -13,11 +13,9 @@ const config: Config = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      }
+      },
     },
   },
-  plugins: [
-    require('flowbite/plugin')
-  ],
+  plugins: [require("flowbite/plugin")],
 }
 export default config
