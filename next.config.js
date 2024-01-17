59e9bef1b989dbb4569dd1d1fd6e145e6a3aca49
blacfli <michaelevan.santoso65@gmail.com>
2024-01-17 16:11:27 +0900

    update next config js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    webpack(config) {
-        config.infrastructureLogging = { debug: /PackFileCache/ }
-        return config;
-    }
-}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     webpack(config) {
+//         config.infrastructureLogging = { debug: /PackFileCache/ }
+//         return config;
+//     }
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+module.exports = {
+    webpack: (config) => {
+      config.resolve.fallback = { fs: false, path: false };
+
+      return config;
+    },
+  };
